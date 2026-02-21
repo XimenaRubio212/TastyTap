c9d9f733aaffaea156a3fc5741ca9c2af6e5211a
ximena <ximenarubio121@gmail.com>
2026-02-20 22:22:46 -0500

    Se realizo cambios en los archivos js login y registro

--- js/login.js
+++ js/login.js
@@ -9,7 +9,6 @@ document.addEventListener('DOMContentLoaded', () => {
         const formData = new URLSearchParams(new FormData(form));
 
         try {
-            // ✅ URL CORRECTA
             const response = await fetch('http://localhost:8080/backend-java-1.0-SNAPSHOT/api/login', {
                 method: 'POST',
                 body: formData,
@@ -21,8 +20,19 @@ document.addEventListener('DOMContentLoaded', () => {
             const data = await response.json();
 
             if (data.status === 'success') {
+                // 1. Separar el token de la URL que enviamos desde Java
+                const [token, redirectUrl] = data.token.split('|');
+
+                // 2. Guardar token en el navegador (Punto 6)
+                localStorage.setItem('tastytap_token', token);
+                localStorage.setItem('user_role', data.data.rol_id); // Guardamos el rol para validaciones rápidas
+
                 alert("👋 " + data.message);
-                window.location.href = '../../views/client/seguir-comprando.html'; 
+
+                // 3. Redirección Dinámica (Puntos 7 y 9)
+                // Usamos la ruta que nos mandó el servidor
+                window.location.href = `../../${redirectUrl}`; 
+                
             } else {
                 alert("❌ " + data.message);
             }
